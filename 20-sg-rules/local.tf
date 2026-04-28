edfc97246236c7185e4a8b25082c0f3265b6d797
SaiThota <thotasaiprasad180@gmail.com>
2026-04-28 13:54:51 +0530

    k8

--- 20-sg-rules/local.tf
+++ 20-sg-rules/local.tf
@@ -6,6 +6,6 @@ locals {
     mysql = data.aws_ssm_parameter.mysql_sg_id.value
     rabbitmq = data.aws_ssm_parameter.rabbitmq_sg_id.value
     ingress_alb = data.aws_ssm_parameter.ingress_alb_sg_id.value
-    eks_control_plane = data.aws_ssm_parameter.eks_control_plane.value
-    eks_node = data.aws_ssm_parameter.eks_node.value
+    eks_control_plane = data.aws_ssm_parameter.eks_control_plane_sg_id.value
+    eks_node = data.aws_ssm_parameter.eks_node_sg_id.value
 }
