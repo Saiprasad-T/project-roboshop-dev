63efb925c83be51a891ce54293dce74d1a608230
SaiThota <thotasaiprasad180@gmail.com>
2026-04-28 14:12:25 +0530

    k8

--- 40-eks/data.tf
+++ 40-eks/data.tf
@@ -3,13 +3,13 @@ data "aws_ssm_parameter" "vpc_id" {
 }
 
 data "aws_ssm_parameter" "private_subnet_ids" {
-    name = "/${var.project}/${var.environment}/private_subnet_ids"
+    name = "/${var.project}/${var.environment}/private-snet"
 }
 
 data "aws_ssm_parameter" "eks_node_sg_id" {
-    name = "/${var.project}/${var.environment}/eks_node_sg_id"
+    name = "/${var.project}/${var.environment}/eks-node-sg-id"
 }
 
 data "aws_ssm_parameter" "eks_control_plane_sg_id" {
-    name = "/${var.project}/${var.environment}/eks_control_plane_sg_id"
+    name = "/${var.project}/${var.environment}/eks-control-plane-sg-id"
 }
