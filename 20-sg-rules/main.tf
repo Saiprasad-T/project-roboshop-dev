e37124df3f814cdf86d43dafd9558d65b0e089d4
SaiThota <thotasaiprasad180@gmail.com>
2026-04-29 13:09:41 +0530

    k8

--- 20-sg-rules/main.tf
+++ 20-sg-rules/main.tf
@@ -159,6 +159,15 @@ resource "aws_security_group_rule" "mongodb_eks_node" {
   security_group_id = local.mongodb
 }
 
+resource "aws_security_group_rule" "mongodb_eks_node" {
+  type              = "ingress"
+  from_port         = 27017
+  to_port           = 27017
+  protocol          = "tcp"
+  source_security_group_id = "sg-05ab8727670bde7cd"
+  security_group_id = local.mongodb
+}
+
 resource "aws_security_group_rule" "redis_eks_node" {
   type              = "ingress"
   from_port         = 6379
